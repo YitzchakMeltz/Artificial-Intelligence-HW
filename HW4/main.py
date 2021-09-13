11ce1ad36e03e6b1fe115b1bf61a2261c3b2c759
YitzchakMeltz <75029003+YitzchakMeltz@users.noreply.github.com>
2021-09-13 22:21:09 +0300

    Update main.py

--- HW4/main.py
+++ HW4/main.py
@@ -1,13 +1,21 @@
+#================================ Q1.0 ================================
 import math
 import csv
 
-class distClass:
-    dist = -1 #distance of current point from test point
-    tag = '-' #tag of current point
+#================================ Q1.1 ================================
+point = [1, 0, 0, '?'] 
+data1 = [1, 1, 1, 'M']
+data2 = [1, 2, 0, 'F']
+
+#================================ Q1.2 ================================
 
 def printVector(vec):
     print('The vector ',vec[:-1],' has tag ',vec[-1])
 
+printVector(data1)
+printVector(data2)
+
+#================================ Q1.3 ================================
 def euclideanDistance(instance1, instance2, length):
    distance = 0
    for x in range(length):
@@ -17,15 +25,9 @@ def euclideanDistance(instance1, instance2, length):
          distance += pow(num1-num2, 2)
    return math.sqrt(distance)
 
-point = [1, 0, 0, '?'] 
-data1 = [1, 1, 1, 'M']
-data2 = [1, 2, 0, 'F']
-
-printVector(data1)
-printVector(data2)
-
 print('Distance between vectors: ',euclideanDistance(data1,data2,3),'\n')
 
+#================================ Q1.4 ================================
 with open('C:/Users/hmeltz/Documents/GitHub/Artificial-Intelligence-HW/HW4/myFile.csv', 'r') as myCsvfile:
     lines = csv.reader(myCsvfile)
     dataWithHeader = list(lines)
@@ -37,11 +39,17 @@ printVector(dataset[0])
 printVector(dataset[1])
 distance = euclideanDistance(dataset[0],dataset[1],3)
 print('Distance between vectors: ',distance,'\n')
+
+#================================ Q1.5 ================================
+class distClass:
+    dist = -1 #distance of current point from test point
+    tag = '-' #tag of current point
+
 obj = distClass()
 
 eucDistances = [] # list of distances, will hold objects of type distClass
+
+point = dataset[0]
 obj.dist = distance
 obj.tag = dataset[1][-1]
 eucDistances.append(obj)
-
-print(type(obj))
